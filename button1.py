2814c4a8813e09490aafa7729195aaea0d2d0ac3
ik <seong9513@gmail.com>
2017-11-15 11:10:46 +0900

    name and std-id

--- button1.py
+++ button1.py
@@ -2,8 +2,8 @@ from tkinter import *
 
 window =Tk()
 
-b1 = Button(window, text="One")
-b2 = Button(window, text="Two")
+b1 = Button(window, text="Hyeockjin Lee")
+b2 = Button(window, text="60142326")
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
 
